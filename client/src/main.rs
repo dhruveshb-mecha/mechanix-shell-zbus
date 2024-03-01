018beb6137e045e78d2932fc644bb2ecb5fd4860
dhruveshb-mecha <dhruveshb@mechasystems.com>
2024-02-29 23:25:37 -0500

    feat: client added

--- client/src/main.rs
+++ client/src/main.rs
@@ -7,6 +7,7 @@ use zbus::{Connection, Result, proxy};
 )]
 trait DisplayBusInterface {
     async fn set_display_brightness(&self, brightness: u8) -> Result<u8>;
+    async fn get_display_brightness(&self) -> Result<u8>;
 }
 
 // Although we use `async-std` here, you can use any async runtime of choice.
@@ -15,9 +16,22 @@ async fn main() -> Result<()> {
     let connection = Connection::session().await?;
 
     // `proxy` macro creates `MyGreeterProxy` based on `Notifications` trait.
+    // let proxy = DisplayBusInterfaceProxy::new(&connection).await?;
+    // let reply = match proxy.set_display_brightness(10).await {
+    //     Ok(brightness) => format!("Brightness set to: {}", brightness),
+    //     Err(e) => {
+    //         eprintln!("Error: {}", e);
+    //         "Failed to set brightness".to_string()
+        
+    //     },
+    // };
+
+
+
+    //get display brightness
     let proxy = DisplayBusInterfaceProxy::new(&connection).await?;
-    let reply = match proxy.set_display_brightness(10).await {
-        Ok(brightness) => format!("Brightness set to: {}", brightness),
+    let reply = match proxy.get_display_brightness().await {
+        Ok(brightness) => format!("Display Brightness is: {}", brightness),
         Err(e) => {
             eprintln!("Error: {}", e);
             "Failed to set brightness".to_string()
