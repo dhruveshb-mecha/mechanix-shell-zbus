2a0d36e600ed274b3d499199312b2bbe9cdd674d
dhruveshb-mecha <dhruveshb@mechasystems.com>
2024-02-29 23:25:14 -0500

    feat: utils added

---
+++ utils/src/lib.rs
@@ -0,0 +1,36 @@
+use std::{fs::File, io::Read};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BaseConfig {
+    pub interfaces: Interfaces,
+}
+
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub struct Interfaces {
+    pub display: Display,
+    pub battery: Battery,
+}
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub struct Display {
+    pub device: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub struct Battery {
+    pub device: String,
+}
+
+
+//write a function to parse the yaml file
+pub fn parse_yaml() -> Result<BaseConfig, Box<dyn std::error::Error>> {
+    //read the yaml file
+    let mut file = File::open("./Confing.yml").expect("Failed to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Failed to read file");
+
+    let base_config: BaseConfig = serde_yaml::from_str(&contents).expect("Failed to deserialize YAML");
+
+    Ok(base_config)
+}
