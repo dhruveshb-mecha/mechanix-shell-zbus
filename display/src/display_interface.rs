0efd670475650d5371e579aeb7c02cb35387a563
dhruveshb-mecha <dhruveshb@mechasystems.com>
2024-03-04 05:13:53 -0500

    feat: display interface added

--- display/src/display_interface.rs
+++ display/src/display_interface.rs
@@ -39,4 +39,50 @@ impl DisplayBusInterface {
 
         result
     }
+
+    //set backlight on
+    pub async fn turn_backlight_on(&self) -> u8 {
+        //get display path
+        let display_path = parse_yaml().unwrap().interfaces.display.device;
+
+        //get display instance
+        let display = DisplayControl { path: display_path };
+
+        //set display brightness to maximum if  there is an error return  by default the sdk returns () we need to return a u8
+        let result = match display.set_display_brightness(244) {
+            Ok(_) => 244,
+            Err(_) => 0,
+        };
+
+        result
+    }
+
+    //set backlight off
+    pub async fn turn_backlight_off(&self) -> u8 {
+        //get display path
+        let display_path = parse_yaml().unwrap().interfaces.display.device;
+
+        //get display instance
+        let display = DisplayControl { path: display_path };
+
+        //set display brightness to minimum if  there is an error return  by default the sdk returns () we need to return a u8
+        let result = match display.set_display_brightness(0) {
+            Ok(_) => 0,
+            Err(_) => 0,
+        };
+
+        result
+    }
+
+    //todo: implement the display interface methods to get timeout
+    pub async fn get_screen_timeout(&self) -> u32 {
+        println!("get screen timeout");
+        0
+    }
+
+    //todo: implement the display interface methods to set timeout
+    pub async fn set_screen_timeout(&self, timeout: u32) -> u32 {
+        println!("get screen timeout");
+        10
+    }
 }
