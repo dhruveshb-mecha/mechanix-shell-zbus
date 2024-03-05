7d195a88692b6eef41b9554f94b00252f8c321f6
dhruveshb-mecha <dhruveshb@mechasystems.com>
2024-03-04 23:52:38 -0500

    chore: power crate error handling added

--- power/src/power_interface.rs
+++ power/src/power_interface.rs
@@ -1,14 +1,39 @@
 use std::process::Command;
 
-use mecha_battery_ctl::{Battery, BatteryControl, PowerSupplyInfo};
+use mecha_battery_ctl::{Battery, PowerSupplyInfo};
 use utils::parse_yaml;
-use zbus::interface;
+use zbus::{
+    interface,
+    zvariant::{DeserializeDict, SerializeDict, Type},
+};
+
+use crate::handle_power_supply_error;
 
 pub struct PowerBusInterface {}
 
+#[derive(DeserializeDict, SerializeDict, Type)]
+// `Type` treats `BatteryStatusZbus` is an alias for `a{sv}`.
+#[zvariant(signature = "a{sv}")]
+pub struct BatteryStatusZbus {
+    pub name: String,
+    pub r#type: String,
+    pub status: String,
+    pub present: bool,
+    pub voltage_now: u32,
+    pub current_now: i32,
+    pub capacity: u8,
+    pub capacity_level: String,
+    pub temp: i32,
+    pub technology: String,
+    pub charge_full: u32,
+    pub charge_now: u32,
+    pub charge_full_design: u32,
+    pub manufacturer: String,
+}
+
 #[interface(name = "Mechanix.Services.Power")]
 impl PowerBusInterface {
-    pub async fn get_battery_status(&self) -> String {
+    pub async fn get_battery_status(&self) -> Result<BatteryStatusZbus, zbus::fdo::Error> {
         //get battery path
         let battery_path = parse_yaml().unwrap().interfaces.battery.device;
 
@@ -18,35 +43,34 @@ impl PowerBusInterface {
             currnet_now: format!("{}/current_now", battery_path),
         };
 
-        //moke PowerSupplyInfo object
-        let power_supply_info = BatteryControl {
-            name: "BAT0".to_string(),
-            r#type: "Battery".to_string(),
-            status: "Discharging".to_string(),
-            present: true,
-            voltage_now: 12000,
-            current_now: -1000,
-            capacity: 80,
-            capacity_level: "High".to_string(),
-            temp: 30,
-            technology: "Li-ion".to_string(),
-            charge_full: 10000,
-            charge_now: 8000,
-            charge_full_design: 10000,
-            manufacturer: "LG".to_string(),
-        };
-
         //get battery status if  there is an error return moke BatteryInfo
         let result = match battery.info() {
             Ok(status) => status,
-            Err(_) => power_supply_info,
+            Err(e) => {
+                return Err(handle_power_supply_error(e));
+            }
         };
 
-        result.status
+        Ok((BatteryStatusZbus {
+            name: result.name,
+            r#type: result.r#type,
+            status: result.status,
+            present: result.present,
+            voltage_now: result.voltage_now,
+            current_now: result.current_now,
+            capacity: result.capacity,
+            capacity_level: result.capacity_level,
+            temp: result.temp,
+            technology: result.technology,
+            charge_full: result.charge_full,
+            charge_now: result.charge_now,
+            charge_full_design: result.charge_full_design,
+            manufacturer: result.manufacturer,
+        }))
     }
 
     //get battery percentage
-    pub async fn get_battery_info(&self) -> u8 {
+    pub async fn get_battery_info(&self) -> Result<u8, zbus::fdo::Error> {
         //get battery path
         let battery_path = parse_yaml().unwrap().interfaces.battery.device;
 
@@ -59,14 +83,16 @@ impl PowerBusInterface {
         //get battery percentage if  there is an error return  by default the sdk returns () we need to return a u8
         let result: u8 = match battery.info() {
             Ok(battery_info) => battery_info.capacity,
-            Err(_) => 45,
+            Err(e) => {
+                return Err(handle_power_supply_error(e));
+            }
         };
 
-        result
+        Ok(result)
     }
 
     //get power usage
-    pub async fn get_power_usage(&self) -> i32 {
+    pub async fn get_power_usage(&self) -> Result<i32, zbus::fdo::Error> {
         //get battery path
         let battery_path = parse_yaml().unwrap().interfaces.battery.device;
 
@@ -79,14 +105,16 @@ impl PowerBusInterface {
         //get power usage if  there is an error return  by default the sdk returns () we need to return a u8
         let result: i32 = match battery.info() {
             Ok(battery_info) => battery_info.current_now,
-            Err(_) => 45,
+            Err(e) => {
+                return Err(handle_power_supply_error(e));
+            }
         };
 
-        result
+        Ok(result)
     }
 
     // get cpu governor
-    pub async fn get_cpu_governor(&self) -> String {
+    pub async fn get_cpu_governor(&self) -> Result<String, zbus::fdo::Error> {
         //get cpu path
         let cpu_path = parse_yaml().unwrap().interfaces.cpu.device;
 
@@ -96,13 +124,22 @@ impl PowerBusInterface {
             .output()
             .expect("Failed to execute cat command");
 
-        let governor = String::from_utf8_lossy(&output.stdout).to_string();
-
-        governor
+        //on success return governor
+        let _ = match output.status.success() {
+            true => {
+                let governor = String::from_utf8(output.stdout).unwrap();
+                return Ok(governor);
+            }
+            false => {
+                return Err(zbus::fdo::Error::Failed(
+                    "Failed to get governor".to_string(),
+                ));
+            }
+        };
     }
 
     // set cpu governor
-    pub async fn set_cpu_governor(&self, governor: &str) -> String {
+    pub async fn set_cpu_governor(&self, governor: &str) -> Result<String, zbus::fdo::Error> {
         //get cpu path
         let cpu_path = parse_yaml().unwrap().interfaces.cpu.device;
 
@@ -115,12 +152,16 @@ impl PowerBusInterface {
                 .expect("Failed to execute echo command");
 
             if result.status.success() {
-                return "Governor set successfully".to_string();
+                return Ok("Governor set successfully".to_string());
             } else {
-                return "Failed to set governor".to_string();
+                return Err(zbus::fdo::Error::Failed(
+                    "Failed to set governor".to_string(),
+                ));
             }
         } else {
-            return "Invalid governor value".to_string();
+            return Err(zbus::fdo::Error::InvalidArgs(
+                "Invalid governor value".to_string(),
+            ));
         }
     }
 }
